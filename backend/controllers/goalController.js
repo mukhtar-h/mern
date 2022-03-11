a9af7c15af69507f7e42a8325cfceeceee4901a8
mukhtar-h <mukhtarkhagan@gmail.com>
2022-03-11 21:35:35 +0500

    REST API for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -13,14 +13,18 @@ const getGoals = asyncHandler(async (req, res) => {
 // @route   POST /api/goals/
 // @access  Private
 const setGoal = asyncHandler(async (req, res) => {
+
     if (!req.body.text) {
         res.status(400)
         throw new Error('please add a text field')
     }
+
     const goal = await Goal.create({
         text: req.body.text,
     })
+
     res.status(200).json(goal)
+
 })
 
 // @des     Update goal
@@ -34,7 +38,7 @@ const updateGoal = asyncHandler(async (req, res) => {
         throw new Error('goal not found')
     }
 
-    await goal.remove()
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true, })
 
     res.status(200).json(updatedGoal)
 })
@@ -51,7 +55,7 @@ const deleteGoal = asyncHandler(async (req, res) => {
         throw new Error('goal not found')
     }
 
-    const deletedGoal = await Goal.findByIdAndDelete(req.params.id)
+    await goal.remove()
 
     res.status(200).json({
         message: 'goal deleted'
